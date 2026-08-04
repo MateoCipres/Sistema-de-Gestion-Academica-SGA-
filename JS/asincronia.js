b049cc94b00c5c750d652e9dc9d0c5ac66b6f6f0
MateoCipres <matoecipres7@gmail.com>
2026-08-04 16:25:12 -0300

    02 ejercicio asincronia

--- JS/asincronia.js
+++ JS/asincronia.js
@@ -1,41 +1,53 @@
-console.log("Inicio")
+// console.log("Inicio")
+// setTimeout(() => {
+//     console.log("Buscando alumnos...")
+// }, 3000)
+
+// console.log("Fin")
+
+// function saludar() {
+//     console.log("Hola")
+// }
+
+// function ejecutar(funcion) {
+//     funcion();
+// }
+
+// ejecutar(saludar)
+
+// function despedirse() {
+//     console.log("Hasta luego")
+// }
+// setTimeout(despedirse, 3000)
+
+// setTimeout(() => {
+//     console.log("Buscando docentes...")
+// }, 6000);
+
+// setTimeout(() => {
+//     console.log("Buscando materias...")
+// }, 2000);
+
+// setTimeout(() => {
+//     console.log("Buscando cursos...")
+// }, 1000);
+
+// console.log("Abriendo SGA")
+
+// setTimeout(() => {
+//     console.log("Alumnos cargados")
+// }, 3000);
+
+// console.log("todos los alumnos estan cargados")
+
+// En 5 segundos de espera tiene que aparecer "lista recibida"
+// Solicitando lista de alumnos va primero...
+// "Mientras tanto el programa sigue ejecutandose"
+
 setTimeout(() => {
-    console.log("Buscando alumnos...")
-}, 3000)
+    console.log("lista recibida")
+}, 5000);
 
-console.log("Fin")
+console.log("Solicitando lista de alumnos...")
 
-function saludar() {
-    console.log("Hola")
-}
-
-function ejecutar(funcion) {
-    funcion();
-}
-
-ejecutar(saludar)
-
-function despedirse() {
-    console.log("Hasta luego")
-}
-setTimeout(despedirse, 3000)
-
-setTimeout(() => {
-    console.log("Buscando docentes...")
-}, 6000);
-
-setTimeout(() => {
-    console.log("Buscando materias...")
-}, 2000);
-
-setTimeout(() => {
-    console.log("Buscando cursos...")
-}, 1000);
-
-console.log("Abriendo SGA")
-
-setTimeout(() => {
-    console.log("Alumnos cargados")
-}, 3000);
-
-console.log("todos los alumnos estan cargados")
+console.log("El programa sigue ejecutandose")
