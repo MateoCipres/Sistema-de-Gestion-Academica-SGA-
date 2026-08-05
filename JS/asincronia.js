a7fb0b7354978b6aec54d807dd88bd129b791315
MateoCipres <matoecipres7@gmail.com>
2026-08-05 15:51:44 -0300

    clase 03

--- JS/asincronia.js
+++ JS/asincronia.js
@@ -44,10 +44,69 @@
 // Solicitando lista de alumnos va primero...
 // "Mientras tanto el programa sigue ejecutandose"
 
-setTimeout(() => {
-    console.log("lista recibida")
-}, 5000);
+// setTimeout(() => {
+//     console.log("lista recibida")
+// }, 5000);
 
-console.log("Solicitando lista de alumnos...")
+// console.log("Solicitando lista de alumnos...")
 
-console.log("El programa sigue ejecutandose")
+// console.log("El programa sigue ejecutandose")
+
+// function obtenerAlumnos() {
+//     console.log("Ya tengo el arreglo")
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve(["Ana", "Laura", "Maria"])
+//         }, 3000);
+//     })
+// }
+
+// // obtenerAlumnos().then((alumnos) => {
+// //     console.log(alumnos)
+// // })
+
+// async function iniciar() {
+//     const alumnos = await obtenerAlumnos()
+//     console.log(alumnos)
+// }
+// iniciar()
+
+function obtenerClima() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("22°C - Soleado");
+        }, 2000);
+    });
+}
+
+//con Then
+obtenerClima().then((clima) => {
+    console.log(clima)
+});
+
+async function mostrarClima() {
+    const clima = await obtenerClima()
+    console.log(clima)
+}
+
+mostrarClima()
+
+function consultarSaldo() {
+    return new Promise((resolve))
+    setTimeout(() => {
+        resolve(125000)
+    }, 3000);
+}
+
+function iniciarSesion() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Bienvenidos Mateo")
+        }, 2000);
+    })
+
+}
+async function iniciarSesion() {
+    const mensaje = await iniciando()
+    console.log(mensaje)
+}
