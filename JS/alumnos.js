01a2bcb4b33e33cd86c27deb3effcc093ecb9a68
MateoCipres <matoecipres7@gmail.com>
2026-08-10 15:35:07 -0300

    clase 05-localStorage

--- JS/alumnos.js
+++ JS/alumnos.js
@@ -1,17 +1,27 @@
-// async function obtenerAlumnos() {
-//     const respuesta = await fetch("https://jsonplaceholder.typicode.com/users")
-//     const alumnos = await respuesta.json()
-//     return alumnos
-// }
+async function obtenerAlumnos() {
+    const respuesta = await fetch("https://jsonplaceholder.typicode.com/users")
+    const alumnos = await respuesta.json()
+    return alumnos
+}
 
-// function mostrarAlumnos(alumnos) {
-//     // console.table(alumnos)
-//     // console.log(alumnos[0].name)
-//     for (const alumno of alumnos) {
-//         console.log(alumno.id, alumno.name, alumno.email)
-//     }
+function mostrarAlumnos(alumnos) {
+    console.log(typeof alumnos)
+    localStorage.setItem("alumnos", JSON.stringify(alumnos))
+    const datos = localStorage.getItem("alumnos")
+    console.log(typeof datos)
+    console.log(datos)
+    const alumnosRecuperados = JSON.parse(datos)
+    console.log(typeof alumnosRecuperados)
 
-// }
+    console.table(alumnosRecuperados)
+    console.log(alumnosRecuperados[0].name)
+    for (const alumno of alumnosRecuperados) {
+        console.log(alumno.id, alumno.name, alumno.email)
+    }
+}
+
+obtenerAlumnos().then(alumnos => mostrarAlumnos(alumnos))
+
 
 // async function iniciar() {
 //     const alumnos = await obtenerAlumnos()
@@ -22,21 +32,21 @@
 
 //traer de json del post el id  y body y comments  
 
-async function obtenerPosteos() {
-    const mostrar = await fetch("https://jsonplaceholder.typicode.com/posts")
-    const posts = await mostrar.json()
-    return posts
-}
+// async function obtenerPosteos() {
+//     const mostrar = await fetch("https://jsonplaceholder.typicode.com/posts")
+//     const posts = await mostrar.json()
+//     return posts
+// }
 
-function mostrarPosteos(posts) {
-    console.table(posts)
-    for (const posteo of posts) {
-        console.log(posteo.id, posteo.body)
-    }
-}
+// function mostrarPosteos(posts) {
+//     console.table(posts)
+//     for (const posteo of posts) {
+//         console.log(posteo.id, posteo.body)
+//     }
+// }
 
-async function iniciar() {
-    const posts = await obtenerPosteos()
-    mostrarPosteos(posts)
-}
-iniciar()
+// async function iniciar() {
+//     const posts = await obtenerPosteos()
+//     mostrarPosteos(posts)
+// }
+// iniciar()
